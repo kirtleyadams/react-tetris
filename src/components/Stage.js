7ae957769523b29e7277da45d1a30e4d0f6b488c
kirtleyadams <hello@kirtleymichelle.com>
2022-05-06 14:25:45 -0400

    shapes:

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -2,8 +2,8 @@ import Cell from './Cell';
 
 const Stage = ({ stage }) => (
     <div>
-        <Cell/>
+        {stage.map (row => row.map((cell,x) => <Cell key= {x} type={cell[0]}/>))}
     </div>
-)
+);
 
 export default Stage;
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,12 +1,13 @@
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
+import {createStage} from '../gameHelpers';
 
 const Tetris = () => {
 
     return (
         <div>
-            <Stage/>
+            <Stage stage={createStage()}/>
             <aside>
                 <div>
                     <Display text ="Score"/>
